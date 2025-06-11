a0cf1f1c9d4883054fd111db4e1f86a3173b1546
Gleb Kapustin <gleb.kapustin@outlook.com>
2025-06-11 23:40:40 +0300

    123

--- obj/Debug/net8.0/First cods in university.AssemblyInfo.cs
+++ obj/Debug/net8.0/First cods in university.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("First cods in university")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24998e72bdf27a18fb5a2660ef667b049421487b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+191ea4cba8e178fed13f0434b3720bd343e4614a")]
 [assembly: System.Reflection.AssemblyProductAttribute("First cods in university")]
 [assembly: System.Reflection.AssemblyTitleAttribute("First cods in university")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
