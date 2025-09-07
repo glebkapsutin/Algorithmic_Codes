45ad75bff4419e2831aa9f0514af8b6e6f80ae2e
Gleb <gleb.kapustin1998@gmail.com>
2025-09-07 12:00:21 +0300

    123

---
+++ BaseAlgorithmic.cs
@@ -0,0 +1,45 @@
+namespace First_cods_in_university.Sort
+{
+    public class Algorithmic
+    {
+        public static int LinerSearch(int[] a , int t)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                if(a[i] == t)
+                {
+                    return i;
+                }
+
+            }
+            return -1;
+        }
+        public static int BinarySearch(int[] a , int t )
+        {   
+            int left = 0;
+             
+            int right = a.Length - 1;
+
+            while(left<= right)
+            {
+                int middle = (left+ right ) / 2;
+
+                if(a[middle] == t)
+                {
+                    return middle;
+                }
+               if(a[middle] < t)
+               {
+                left = middle +1;
+               }
+               else{
+                right = middle - 1;
+               }
+
+            }
+            return -1;
+        }
+
+    }
+   
+}
