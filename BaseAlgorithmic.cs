10765d40855bb5218bcd3d964c05664201876fe9
Gleb <gleb.kapustin1998@gmail.com>
2025-09-09 17:47:15 +0300

    1234

--- BaseAlgorithmic.cs
+++ BaseAlgorithmic.cs
@@ -1,3 +1,5 @@
+using System.Security.AccessControl;
+
 namespace First_cods_in_university.Sort
 {
     public class Algorithmic
@@ -66,6 +68,73 @@ namespace First_cods_in_university.Sort
             }
 
         }
+        public static int BinarySearch2(int[] a , int t)
+        {   
+            
+            int right = a.Length-1;
+            int left = 0;
+           
+            while (left<=right)
+            {   
+                int middle = (left+right) / 2;
+                if (a[middle] ==t)
+                {
+                    return middle;
+                }
+                if (t > a[middle])
+                {
+                    left = middle +1;
+                }
+                else{
+                    right = middle -1;
+                }
+            }
+            return -1;
+        }
+        public static void BubleSort2(int[] a)
+        {
+             int n  = a.Length;
+
+             for (int i = 0; i < n-1; i++)
+             {   
+                bool swaped = false;
+                for (int j = 0; j < n-i-1; j++)
+                {   
+                   
+                    if (a[j] > a[j+1])
+                    {
+                        int temp = a[j];
+                        a[j] = a[j+1];
+                        a[j+1] = temp;
+                        swaped = true;
+                    }
+                }
+                if(!swaped)
+                {
+                    break;
+                }
+             }
+
+        }
+        public static void InsertionSort(int[] a )
+        {
+            int n = a.Length;
+            for (int i = 1; i < n; i++)
+            {
+                int key = a[i];
+                int j = i -1;
+
+                while(j>= 0 && a[j] > key )
+                {
+                    a[j+1] = a[j];
+                    j--;
+        
+                }
+                a[j+1] = key;
+            }
+
+        }
+
     }
    
 }
